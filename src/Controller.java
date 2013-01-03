28ec1815c3e1fa3cd7b9feed6fe7c5d70c059466
Zack Holmgren <zack.holmgren@gmail.com>
2013-01-03 16:23:00 +0100

    Finished instruction model and created a small testfile

--- src/Controller.java
+++ src/Controller.java
@@ -1,11 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+
+import Simulation.Instruction;
+import Simulation.Processor;
+
 
 
 public class Controller {
 	private GUI gui;
+	private DefaultListModel instructionModel;
 	
 	public Controller() {
 		gui = new GUI();
 		gui.setGUIListener(listener);
+		
+		instructionModel = new DefaultListModel();
+		gui.setInstructionListModel(instructionModel);
 	}
 	
 	private GUI.GUIListener listener = new GUI.GUIListener() {
@@ -36,8 +51,38 @@ public class Controller {
 		
 		@Override
 		public void onLoad(String filename) {
-			// TODO Auto-generated method stub
+			String line;
+			BufferedReader reader = null;
+			ArrayList<Instruction> instructions = new ArrayList<Instruction>();
 			
+			instructionModel.clear();
+			
+			try {
+				reader = new BufferedReader(new FileReader(filename));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			try {
+				while((line = reader.readLine()) != null){
+					Instruction instruction = new Instruction(line);
+					instructions.add(instruction);
+					instructionModel.addElement(instruction);
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		//	TODO new Processor().setInstructionSet(instructions);
 		}
 		
 		@Override
