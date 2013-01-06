f7babc24de8a20c3a6bdb5c14e32e90bb38a16ba
Orvar Segerström <OrvarSegerstrom@gmail.com>
2013-01-06 22:36:22 +0100

    Corrected NOR ALU control number

--- src/mips/ALU.java
+++ src/mips/ALU.java
@@ -3,11 +3,10 @@ package mips;
 public class ALU {
 	public static final short AND = 0;
 	public static final short OR = 1;
-
 	public static final short ADD = 2;
-	public static final short NOR = 3;
 	public static final short SUBTRACT = 6;
 	public static final short SLT = 7;
+	public static final short NOR = 12;
 
 	public int operation(short control, int srcv, int rsv) {
 		switch(control) {
